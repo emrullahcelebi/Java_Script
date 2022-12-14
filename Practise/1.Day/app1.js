6672daadd017e8104442e89870d861f366b4c8cf
Emrullah Celebi <cumali20182019@gmail.com>
2022-12-14 22:13:32 +0100

    14.12.22

---
+++ Practise/1.Day/app1.js
@@ -0,0 +1,88 @@
+//Ornek-1
+// console.log("Javascript ilk ders");
+// console.warn("Uyarilari gosterir");
+// console.error("Hatalari gosterme amacli");
+
+
+//Ornek-2
+//alert("bu bir uyari mesajidir")
+//console.log(confirm("Silmek istediginizden emin misiniz?"));
+
+
+//Ornek-3
+
+// let uzunKenar=prompt("Uzun kenari giriniz");//deger aliyoruz
+// let kisaKenar=prompt("Kisa kenari giriniz");
+// let alan = Number(uzunKenar) * Number(kisaKenar);
+// console.log(`Dikdortgenin alani = ${alan}'dir. `);
+// alert(`Dikdortgenin alani = ${alan}'dir. `);
+
+//Ornek-4
+
+// let pi=3.14;
+// console.log(typeof(pi));
+
+// let deger=true;
+// console.log(typeof(deger));
+
+//Ornek-5
+
+// let ad1 = "Ali";
+// let ad2 = "Oya";
+// let ad3 = "Can";
+// let ad4 = null;
+// let ad5 = "";
+// let ad6 = undefined;
+
+// console.log(ad1 && ad2 && ad3); //Can
+// console.log(ad1 && ad2 && ad4 && ad3);//null
+// console.log(ad4 || ad5 || ad2 || ad6);//Oya
+// console.log(ad4 || ad5 || false || ad6);//undefined
+
+
+//Ornek-6
+
+// let x=40;
+// let y="30";
+// console.log(x=="40");//true
+// console.log(y===30n);//false
+// console.log(y!==30n);//true
+// console.log(x>(y+10));//false
+// console.log(x<(y+10));//true
+
+//Ornek-7
+
+// const sayi= 254;
+
+// const birler = sayi%10;
+// const onlar= Math.floor(sayi/10)%10;
+// const yuzler = Math.floor(sayi/100)%10;
+// const toplam = birler + onlar + yuzler;
+// console.log(`${sayi} sayisinin rakamlar toplami = ${toplam}`);
+
+
+//Ornek-8
+// let a=5;
+// let b =--a;
+// let c=b++;
+// console.log(a,b,c);
+
+
+//Ornek-9
+
+    // const sayi1 = 5;true
+    // const sayi2=-7;true
+    // const isim = "John";true
+    // const sifir=0;//false
+    // const hic=null;//false
+    // const bos="";//false
+    // const tanimsiz=undefined;//false
+    // const sayiDegil=NaN;//false
+    // console.log(Boolean(sayi1));//true
+    // console.log(Boolean(bos),Boolean(hic),Boolean(sifir),Boolean(tanimsiz),Boolean(sayiDegil));
+
+
+    //Ornek-10
+
+    
+
