4ee827b909758d4107f56d9634061be649985de0
Emrullah Celebi <cumali20182019@gmail.com>
2022-12-23 21:49:12 +0100

    23.12.22

--- Practise/Advence-JavaScript/1.Day/tas-kagit-makas/2.yol/app.js
+++ Practise/Advence-JavaScript/1.Day/tas-kagit-makas/2.yol/app.js
@@ -10,3 +10,62 @@
  switch ile yazılması
 Append sayfada seçtiğimiz bir alanın sonuna veri eklememizi sağlar.
  */
+
+
+const kullaniciSecimiEl = document.createElement("h2");
+const sanalSecimEl= document.createElement("h2");
+const sonucEl= document.createElement("h2");
+const containerEl= document.getElementById("container");
+containerEl.append(kullaniciSecimiEl,sanalSecimEl,sonucEl);
+
+const secenekler=["tas","kagit","makas"];
+
+
+let kullaniciSecimi;
+let sanalSecim;
+
+
+const handleClick = (e) =>{
+    kullaniciSecimi = e.target.id;
+    kullaniciSecimiEl.innerHTML = `Senin secimin: ${kullaniciSecimi}`;
+    generateSanalSecim();
+    getSonuc();
+}
+
+const generateSanalSecim =()=>{
+    sanalSecim=secenekler[Math.floor(Math.random()*secenekler.length)];
+    sanalSecimEl.innerHTML = `Bilgisayarin secimin: ${sanalSecim}`;
+}
+
+/* Butonlari ekliyelim */
+for(let i=0; i<secenekler.length;i++){
+    const buton=document.createElement("button");
+    buton.setAttribute("id",secenekler[i]);
+    buton.innerHTML=secenekler[i];
+    buton.addEventListener("click",handleClick);
+    containerEl.appendChild(buton);
+}
+
+const getSonuc = () => { 
+    switch(kullaniciSecimi + sanalSecim){
+        case "makaskagit":
+        case "tasmakas" :
+        case "kagittas" :
+            sonucEl.innerHTML ="SEN KAZANDIN";
+            break;
+
+        case "kagitmakas":
+        case "makastas" :
+        case "taskagit" :
+            sonucEl.innerHTML ="KAYBETTIN";
+            break;
+
+        case "kagitkagit":
+        case "makasmakas" :
+        case "tastas" :
+            sonucEl.innerHTML ="BER";
+            break;
+        
+
+    }
+ }
--- Practise/Advence-JavaScript/1.Day/tas-kagit-makas/3.yol/app.js
+++ Practise/Advence-JavaScript/1.Day/tas-kagit-makas/3.yol/app.js
@@ -5,3 +5,47 @@
  switch ile yazılması
 Append sayfada seçtiğimiz bir alanın sonuna veri eklememizi sağlar.
  */
+
+const sonucEl =document.querySelector("#sonuc");
+const secenekEl=document.querySelector("#secenek");
+
+const secenekler=["tas","kagit","makas"];
+
+
+
+
+
+
+const getSonuc = (e) => { 
+    let kullaniciSecimi = e.target.innerHTML;
+    let sanalSecim=secenekler[Math.floor(Math.random()*secenekler.length)];
+    switch(kullaniciSecimi + sanalSecim){
+        case "makaskagit":
+        case "tasmakas" :
+        case "kagittas" :
+            sonucEl.innerHTML ="SEN KAZANDIN";
+            break;
+
+        case "kagitmakas":
+        case "makastas" :
+        case "taskagit" :
+            sonucEl.innerHTML ="KAYBETTIN";
+            break;
+
+        case "kagitkagit":
+        case "makasmakas" :
+        case "tastas" :
+            sonucEl.innerHTML ="BERABERE";
+            break;
+        
+
+    }
+ }
+
+ /* Butonlari ekliyelim */
+secenekler.forEach((secim)=>{
+    const buton=document.createElement("button");
+    buton.innerHTML=secim;
+    buton.addEventListener("click",getSonuc);
+    secenekEl.appendChild(buton);
+})
