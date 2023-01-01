15be14db2363917314c36f8ab174ec900b9ecd1d
Emrullah Celebi <cumali20182019@gmail.com>
2023-01-01 15:50:47 +0100

    v1

---
+++ Practise/8.Day/practise-23/app-23.js
@@ -0,0 +1,25 @@
+function sicaklikHesaplama(){
+    let tempSecim = document.querySelector("#sayi_deger").value;
+    let numberTemp=document.querySelector("#deger").value;
+
+    let result;
+
+    if(tempSecim ==1){
+        result = celVeFah(numberTemp);
+        document.querySelector(".sonuc").innerHTML = `${result} Fahrenheit`;
+    }
+    else{
+        result = fahVeCel(numberTemp);
+        document.querySelector(".sonuc").innerHTML = `${result} Celsius`;
+    }
+}
+
+function celVeFah(celcius){
+    let fahrenheit = Math.round(celcius*9/5 + 32);
+    return fahrenheit;
+}
+
+function fahVeCel(fahrenheit){
+    let celcius = Math.round((fahrenheit-32)*5/9);
+    return celcius;
+}
---
+++ Practise/8.Day/rakam_tahnin/app.js
@@ -0,0 +1,57 @@
+let baslat = document.querySelector("#baslat");
+let yukari = document.querySelector("#yukari");
+let asagi = document.querySelector("#asagi");
+let bitir = document.querySelector("#bitir");
+let mesaj = document.querySelector("#mesaj");
+
+let hak = 0,
+  alt = 1,
+  ust = 100,
+  tahmin = 0;
+
+baslat.addEventListener("click", () => {
+  hak = 0;
+  alt = 1;
+  ust = 100;
+  tahmin = 0;
+  baslat.disabled = true;
+  yukari.disabled = false;
+  asagi.disabled = false;
+  bitir.disabled = false;
+
+  sayiTahmin();
+});
+
+const sayiTahmin = () => {
+  tahmin = Math.floor(Math.random() * (ust - alt + 1) + alt);
+  hak++;
+  mesaj.innerHTML = `bilgisayarin tahmini : ${tahmin} <br><br>
+                    <span style="color:red;">Kalan Hak: ${10 - hak}</span>`;
+
+  if (hak == 10) {
+    mesaj.innerHTML = "Hakkiniz doldu, oyunu kaybettiniz.";
+    oyunuBitir();
+  }
+};
+
+yukari.addEventListener("click", () => {
+  alt = tahmin + 1;
+  sayiTahmin();
+});
+
+asagi.addEventListener("click", () => {
+  ust = tahmin - 1;
+  sayiTahmin();
+});
+
+bitir.addEventListener("click", () => {
+  mesaj.innerHTML = `Tebrikler, ${hak}. hakkinizda oyunu kazandiniz.`;
+  oyunuBitir();
+});
+
+const oyunuBitir = () => {
+  baslat.disabled = false;
+  yukari.disabled = true;
+  asagi.disabled = true;
+  bitir.disabled = true;
+};
